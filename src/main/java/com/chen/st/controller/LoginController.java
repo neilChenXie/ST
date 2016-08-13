97356e007d4cd871bb822133b5768acc93eab7d7
Chen Xie <xiec@usc.edu>
2016-08-13 21:25:30 +0800

    shiro exp finished

--- src/main/java/com/chen/st/controller/LoginController.java
+++ src/main/java/com/chen/st/controller/LoginController.java
@@ -7,13 +7,19 @@ import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/manage")
 public class LoginController {
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String Login() {
+		return "login";
+	}
 	
-	@RequestMapping("/login")
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	@ResponseBody
 	public boolean login() {
 		String error = null;
@@ -22,21 +28,21 @@ public class LoginController {
 		try {
 			subject.login(token);
 			subject.getSession().setAttribute("uid", "14532123");
-		} catch (UnknownAccountException e) {  
-            error = "用户名/密码错误";  
-        } catch (IncorrectCredentialsException e) {  
-            error = "用户名/密码错误";  
-        }
+		} catch (UnknownAccountException e) {
+			error = "用户名/密码错误";
+		} catch (IncorrectCredentialsException e) {
+			error = "用户名/密码错误";
+		}
 		System.out.println(error);
 		return subject.isAuthenticated();
 	}
-	
+
 	@RequestMapping("/failed")
 	@ResponseBody
 	public String failed() {
 		return "login failed";
 	}
-	
+
 	@RequestMapping("/logout")
 	@ResponseBody
 	public String logout() {
---
+++ src/main/webapp/jsp/login.jsp
@@ -0,0 +1,7 @@
+<html>
+<body>
+	<form action="/manage/login" method="POST">
+		<input type="submit" value="Login" />
+	</form>
+</body>
+</html>
